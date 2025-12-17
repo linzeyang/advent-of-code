bba4bf932f89a8cc54bd5b023ef7541082734efe
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2025-12-17 14:30:34 +0800

    Year 2025 - Day 10 - part 2

--- 2025/day_10.py
+++ 2025/day_10.py
@@ -1,3 +1,12 @@
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#   "z3-solver>=4.15.4.0",
+# ]
+# ///
+# reference: https://packaging.python.org/en/latest/specifications/inline-script-metadata/
+# usage: uv run day_10.py
+
 """
 day_10.py
 
@@ -9,6 +18,8 @@ https://adventofcode.com/2025/day/10
 from itertools import combinations
 from pathlib import Path
 
+import z3
+
 DATA_PATH: Path = Path(__file__).parent / "data"
 
 
@@ -16,7 +27,7 @@ class Solution:
     def __init__(self) -> None:
         """initiation"""
 
-        self.entries: list = []
+        self.entries: list[tuple[int, list[int], list[list[str]], list[int]]] = []
 
         with DATA_PATH.joinpath("10.txt").open("r", encoding="utf-8") as file:
             for line in file:
@@ -25,10 +36,25 @@ class Solution:
     def _process_entry(
         self, line: str
     ) -> tuple[int, list[int], list[list[str]], list[int]]:
+        """
+        Process a single entry from the input file.
+
+        Args:
+            line (str): A single line from the input file.
+
+        Returns:
+            tuple[int, list[int], list[list[str]], list[int]]: A tuple containing:
+                - light (int): The light status represented as an integer
+                - buttons (list[int]): A list of button statuses represented as integers
+                - button_bits (list[list[str]]): A list of button statuses represented
+                    as binary strings
+                - joltage_req (list[int]): A list of joltage requirements
+        """
+
         splits: list[str] = line.split(" ")
 
         light_diagram: str = splits[0][1:-1]
-        light: int = int(light_diagram.replace(".", "0").replace("#", "1"), 2)
+        light: int = int(light_diagram.replace(".", "0").replace("#", "1"), base=2)
 
         buttons: list[int] = []
         button_bits: list[list[str]] = []
@@ -40,7 +66,7 @@ class Solution:
                 bits[int(num)] = "1"
 
             button_bits.append(bits)
-            buttons.append(int("".join(bits), 2))
+            buttons.append(int("".join(bits), base=2))
 
         joltage_req: list[int] = list(map(int, splits[-1][1:-1].split(",")))
 
@@ -62,8 +88,6 @@ class Solution:
         out: int = 0
 
         for light, buttons, _, _ in self.entries:
-            breaked: bool = False
-
             # Try every combination of buttons, starting from 1 button
             # and up to all buttons
             for k in range(1, len(buttons) + 1):
@@ -75,18 +99,56 @@ class Solution:
 
                     if status == light:
                         out += k
-                        breaked = True
                         break
+                else:  # No valid combination found for this k, jump to next k
+                    continue
 
-                if breaked:
-                    break
+                break  # Valid combination found for this k, move to next entry
 
         return out
 
     def part2(self) -> int:
-        """part2"""
+        """
+        Z3 Solver (https://pypi.org/project/z3-solver/) for Integer Linear Programming
+        We want to minimize sum(x_i)
+        Subject to: A x = b (over Integers)
+        x_i >= 0
+        """
 
-        return -1
+        out: int = 0
+
+        for _, _, button_bits, joltage_req in self.entries:
+            opt = z3.Optimize()
+
+            # Define integer variables
+            vars: list = [z3.Int(name=f"x_{jdx}") for jdx in range(len(button_bits))]
+
+            # Non-negative constraint
+            for var in vars:
+                opt.add(var >= 0)
+
+            # Add linear equation constraints
+            for idx, joltage in enumerate(joltage_req):
+                # Construct the sum expression for this row
+                # A[r] * x = b[r]
+                row_expr = 0
+
+                for jdx, col in enumerate(button_bits):
+                    if col[idx] == "1":
+                        row_expr += vars[jdx]
+
+                opt.add(row_expr == joltage)
+
+            # Objective: Minimize sum of presses
+            opt.minimize(z3.Sum(vars))
+
+            if opt.check() == z3.sat:
+                model = opt.model()
+                out += model.eval(z3.Sum(vars)).as_long()
+            else:
+                print("No solution found!")
+
+        return out
 
 
 def main() -> None:
